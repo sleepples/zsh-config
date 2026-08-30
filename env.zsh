3eb6bc7ee32e53c9f6c3ec01dced840ac1ec2856
sleepples <sleeppls3@gmail.com>
2026-08-30 15:10:22 -0400

    changed /home/<usrname> to /home/spencer to be more reproducable

--- env.zsh
+++ env.zsh
@@ -1,4 +1,4 @@
-export PATH="/home/dinglebarries/Documents/apps:$PATH"
+export PATH="$HOME/Documents/apps:$PATH"
 export MANPAGER="nvim +Man!"
 export SUDO_EDITOR=/usr/bin/nvim
 export EDITOR=/usr/bin/nvim
